9c949af84b11150e4933259257b9c9a24c161de1
Mayro <deehmyller11@gmail.com>
2021-04-09 01:33:41 -0300

    chore: add global and root styles on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
 
+import Header from './pages/Header'
+
+import GlobalStyles from './styles/GlobalStyles'
+import * as S from './styles/rootStyles'
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <S.Wrapper>
+        <Header />
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+        <GlobalStyles />
+      </S.Wrapper>
+    </>
   )
 }
 
