32a8ec3bed551bc4092a654c1f14c4924d328451
Mayro <deehmyller11@gmail.com>
2021-04-09 01:30:53 -0300

    chore: add header component

---
+++ src/pages/Header/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles'
+
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+
+const Header = () => {
+  return (
+    <S.Container>
+      <Logo />
+    </S.Container>
+  )
+}
+
+export default Header
